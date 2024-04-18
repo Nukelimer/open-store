1f84e05858b79bea335d887235f6e050b6b6dbf9
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-04-18 15:59:12 +0100

    trying aain

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,20 +1,32 @@
 
 import { square } from 'ldrs'
+
+
+
+declare namespace JSX {
+    interface IntrinsicElements {
+      "atom-spinner": any;
+    }
+  }
+  
 function Spinner() {
+
+
+ 
     square.register()
   return (
 
 
     
-<div className=""></div>
-    // <l-square
-    //   size="35"
-    //   stroke="5"
-    //   stroke-length="0.25"
-    //   bg-opacity="0.1"
-    //   speed="1.2" 
-    //   color="black" 
-    // ></l-square>
+
+    <l-square
+      size="35"
+      stroke="5"
+      stroke-length="0.25"
+      bg-opacity="0.1"
+      speed="1.2" 
+      color="black" 
+    ></l-square>
   )
 }
 
