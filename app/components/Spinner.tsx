bfa449dcac5f5d4ef5b858afed1d14cc901a515e
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-04-18 16:20:39 +0100

    okay

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -1,28 +1,17 @@
+import { square } from "ldrs";
 
-import { square } from 'ldrs'
-
-
-
-  
 function Spinner() {
-
-
- 
-    square.register()
+  square.register();
   return (
-
-
-    
-
     <l-square
       size="35"
       stroke="5"
       stroke-length="0.25"
       bg-opacity="0.1"
-      speed="1.2" 
-      color="black" 
-    ></l-square>
-  )
+      speed="1.2"
+      color="black"
+    />
+  );
 }
 
-export default Spinner
+export default Spinner;
