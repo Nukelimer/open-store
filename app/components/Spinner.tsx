d0f5cc2ab98484f046f946521fc0f10607beea6e
Nukelimer <112999689+Nukelimer@users.noreply.github.com>
2024-04-18 15:53:40 +0100

    ////

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -6,15 +6,15 @@ function Spinner() {
 
 
     
-
-    <l-square
-      size="35"
-      stroke="5"
-      stroke-length="0.25"
-      bg-opacity="0.1"
-      speed="1.2" 
-      color="black" 
-    ></l-square>
+<div className=""></div>
+    // <l-square
+    //   size="35"
+    //   stroke="5"
+    //   stroke-length="0.25"
+    //   bg-opacity="0.1"
+    //   speed="1.2" 
+    //   color="black" 
+    // ></l-square>
   )
 }
 
